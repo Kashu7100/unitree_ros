9f5fad123e80a5a49c0204ad83be295e269157bc
Kashu7100 <echo_six0566@yahoo.co.jp>
2022-09-18 18:41:00 -0500

    exp

--- unitree_legged_real/src/exe/ros_udp.cpp
+++ unitree_legged_real/src/exe/ros_udp.cpp
@@ -24,7 +24,7 @@ public:
 
 public:
     Custom()
-        : low_udp(LOWLEVEL),
+        : low_udp(LOWLEVEL, 8090, "192.168.123.10", 8007),
           high_udp(8090, "192.168.123.161", 8082, sizeof(HighCmd), sizeof(HighState))
     {
         high_udp.InitCmdData(high_cmd);
